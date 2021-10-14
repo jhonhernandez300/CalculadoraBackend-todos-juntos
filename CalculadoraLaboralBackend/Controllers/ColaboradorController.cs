2d93db48799c357557a24036004624c592e85896
Jhon <desarrollador1@intevo.com.co>
2021-10-14 07:28:39 -0500

    inicio

---
+++ CalculadoraLaboralBackend/Controllers/ColaboradorController.cs
@@ -0,0 +1,60 @@
+﻿using CalculadoraLaboralBackend.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace CalculadoraLaboralBackend.Controllers
+{
+    //[Route("api/Colaborador")]
+    [Route("api")]
+    [EnableCors("CorsPolicy")]
+    public class ColaboradorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+       
+        public ColaboradorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/RegistrarColaborador       
+        [HttpPost("RegistrarColaborador")]
+        public async Task<ActionResult<Colaborador>> RegistrarColaborador([FromBody] Colaborador colaborador)
+        {
+            _context.Colaborador.Add(colaborador);
+            await _context.SaveChangesAsync();
+            return colaborador;
+        }
+
+        // GET: api/ConsultarColaboradorPorIdentificación/5        
+        [HttpGet("ConsultarColaboradorPorIdentificacion/{numeroDeIdentificacion}")]        
+        public async Task<IActionResult> ConsultarColaboradorPorIdentificacion(int numeroDeIdentificacion)
+        {
+            var result = await _context.Colaborador.Where(i => i.NumeroDeIndentificacion == numeroDeIdentificacion).ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+        
+        // GET: api/GetCodigoInterno/      
+        [HttpGet("GetCodigoInterno")]
+        public string getCodigoInterno()
+        {
+            var guid = Guid.NewGuid();
+            string json = JsonConvert.SerializeObject(guid);
+            return json;
+        }
+
+    }
+}
---
+++ CalculadoraLaboralBackend/Models/Servicio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraLaboralBackend.Models
+{
+    public class Servicio
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DataType(DataType.Text, ErrorMessage = "Debe ser un texto")]
+        public string tecnico { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [DataType(DataType.Text, ErrorMessage = "Debe ser un texto")]
+        public string servicioRealizado { get; set; }                
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public DateTime fechaDeInicio { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]        
+        public int horaDeInicio { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public DateTime fechaDeFinalizacion { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public int horaDeFinalizacion { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [Range(1, 52, ErrorMessage = "Un año tiene entre 1 52 semanas")]
+        public int semanaDelAno { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public int cantidadDeHoras { get; set; }        
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public string tipoDeHora { get; set; }        
+
+
+    }
+}
